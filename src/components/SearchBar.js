f0c7c51631d4d2575d2596b0e4e9121067be9ba0
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-09 15:30:23 +0100

    added initial SearchBar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './SearchBar.css';
+
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component{
+
+};
+
+export default SearchBar;
