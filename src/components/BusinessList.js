6079feb33769ff8009f40faef9c1bff8ff9799df
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-09 14:39:15 +0100

    added BusinessList component

---
+++ src/components/BusinessList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Business from './Business';
+import './BusinessList.css'
+
+class BusinessList extends React.Component {
+  render(){
+    return(
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    )
+  };
+
+};
+
+export default BusinessList;
