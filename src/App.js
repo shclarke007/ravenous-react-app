c1787a5c0097b139bf679b6c04339d2a2400806d
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-09 14:19:19 +0100

    added Business component css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Business from './components/Business';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        
         </header>
+        <div>
+          <Business />
+        </div>
       </div>
     );
   }
