f0f775d1521dc837d04d8ab4f6d390f2e96f3273
Shaneil Clarke <shaneilske@yahoo.com>
2019-05-17 17:51:58 +0100

    added functions to get values from term and location input fields

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,7 +15,9 @@ class SearchBar extends Component {
       location: '', 
       sortBy: 'best_match' 
     }; 
-    this.handleSortByChange = this.handleSortByChange.bind(this);           
+    this.handleSortByChange = this.handleSortByChange.bind(this); 
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);          
   }
 
   getSortByClass(sortByOption) {
@@ -23,6 +25,14 @@ class SearchBar extends Component {
       return 'active'
     }
     return ''; 
+  }
+
+  handleTermChange(event){
+    this.setState({ term: event.target.value });
+  }
+
+  handleLocationChange(event){
+    this.setState({ location: event.target.value });
   }
 
   handleSortByChange(sortByOption){
@@ -43,8 +53,8 @@ class SearchBar extends Component {
           <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+          <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
           <a>Let's Go</a>
