0896f6ecfef39d2dac01e38a389c0a13dde5909e
Shaneil Clarke <shaneilske@yahoo.com>
2019-05-17 18:33:32 +0100

    added handleSearch function for search button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -17,7 +17,8 @@ class SearchBar extends Component {
     }; 
     this.handleSortByChange = this.handleSortByChange.bind(this); 
     this.handleTermChange = this.handleTermChange.bind(this);
-    this.handleLocationChange = this.handleLocationChange.bind(this);          
+    this.handleLocationChange = this.handleLocationChange.bind(this);    
+    this.handleSearch= this.handleSearch.bind(this);      
   }
 
   getSortByClass(sortByOption) {
@@ -25,6 +26,11 @@ class SearchBar extends Component {
       return 'active'
     }
     return ''; 
+  }
+
+  handleSearch(event){
+    this.props.searchZomato(this.state.term, this.state.location, this.state.sortBy);
+    event.preventDefault();
   }
 
   handleTermChange(event){
@@ -57,7 +63,7 @@ class SearchBar extends Component {
           <input placeholder="Where?" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
-          <a>Let's Go</a>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     );
